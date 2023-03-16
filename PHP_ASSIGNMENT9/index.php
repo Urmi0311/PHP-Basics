14819e3f5c31dedfdf7e374344cf53c841161823
Urmi0311 <urmi.kothari@sigmainfo.net>
2023-03-16 15:12:31 +0530

    php assignment

--- PHP_ASSIGNMENT9/index.php
+++ PHP_ASSIGNMENT9/index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Upload PHP File</title>
-<link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<center>
-        <h1>READ CSV FILE USING PHP</H1>
-   <div class=form>
-  <form action="Question1.php" method="POST" enctype="multipart/form-data">
-    <input type="file" id="phpfile" name="phpfile" accept=".csv" />
-    <input type="submit" name="submit" value="Upload" />
+  <center>
+    <h1>READ CSV FILE USING PHP</H1>
+    <div class=form>
+      <form action="Question1.php" method="POST" enctype="multipart/form-data">
+        <input type="file" id="phpfile" name="phpfile" accept=".csv" />
+        <input type="submit" name="submit" value="Upload" />
 
-  </form>
-</div>
-</center>
+      </form>
+    </div>
 
+  </center>
 </body>
